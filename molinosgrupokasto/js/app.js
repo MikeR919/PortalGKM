2b3362ee9b6fa054e8e4e18bd767d200ea7dab6f
MikeR919 <55338994+MikeR919@users.noreply.github.com>
2021-01-18 14:56:19 -0600

    ver. 1.11
    se realiza carga masiva de cambios parte 1

---
+++ molinosgrupokasto/js/app.js
@@ -0,0 +1,67 @@
+
+	new Glider(document.querySelector('.glider_linea'), {
+	  slidesToShow: 2,
+	  slidesToScroll: 1,
+	  draggable: true,
+	  dots: '#dots',
+	  arrows: {
+	    prev: '.glider-prev',
+	    next: '.glider-next'
+	  },
+	  responsive: [
+	    {
+	      // screens greater than >= 775px
+	      breakpoint: 450,
+	      settings: {
+	        // Set to `auto` and provide item width to adjust to viewport
+	        slidesToShow: 2,
+	        slidesToScroll: 2,
+	        itemWidth: 150,
+	        duration: 0.25
+	      }
+	    },{
+	      // screens greater than >= 1024px
+	      breakpoint: 800,
+	      settings: {
+	        slidesToShow: 3,
+	        slidesToScroll: 4,
+	        itemWidth: 150,
+	        duration: 0.25
+	      }
+	    }
+	  ]
+	});
+
+	new Glider(document.querySelector('#glider'), {
+	  slidesToShow: 2,
+	  slidesToScroll: 1,
+	  draggable: true,
+	  dots: '#dot',
+	  arrows: {
+	    prev: '.glider-prev',
+	    next: '.glider-next'
+	  },
+	  responsive: [
+	    {
+	      // screens greater than >= 775px
+	      breakpoint: 450,
+	      settings: {
+	        // Set to `auto` and provide item width to adjust to viewport
+	        slidesToShow: 3,
+	        slidesToScroll: 2,
+	        itemWidth: 150,
+	        duration: 0.25
+	      }
+	    },{
+	      // screens greater than >= 1024px
+	      breakpoint: 800,
+	      settings: {
+	        slidesToShow: 5,
+	        slidesToScroll: 1,
+	        itemWidth: 150,
+	        duration: 0.25
+	      }
+	    }
+	  ]
+	});
+
